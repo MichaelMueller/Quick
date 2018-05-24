af85899e379e6e82c427c1cfe5a4763a08543ac1
mueller <mueller@mbits.info>
2018-05-24 17:43:45 +0200

    GraphStorage finished

--- GraphStorage/Tests/NodeTest.php
+++ GraphStorage/Tests/NodeTest.php
@@ -11,35 +11,58 @@ class NodeTest extends \qck\core\abstracts\Test
 
   public function run( \qck\core\interfaces\AppConfig $config )
   {
-    // Create a Student Register: A University has Students and Teachers. Students and Teachers are related (they teach the students)
-    // with CRUD functionality
-    $this->createTests( $config );
+    $this->Dir = $this->getTempDir( "GraphStorage" );
+    $this->testCreate();
+    $this->testRead();
   }
 
   function testCreate()
   {
+    $MyUniversity = University::create( $this->Dir, "My University" );
+    $ProfSteinberg = Teacher::create( "Prof. Steinberg" );
+    $ProfPipen = Teacher::create( "Prof. Pipen" );
     $Sally = Student::create( "Sally Miller" );
     $John = Student::create( "Jon Smith" );
     $Michael = Student::create( "Michael Jordan" );
 
-    $ProfPipen = Teacher::create( "Prof. Pipen" );
-    $ProfPipen->addStudent( $Sally );
-    $ProfPipen->addStudent( $John );
-    $ProfPipen->addStudent( $Michael );
-
-    $ProfSteinberg = Teacher::create( "Prof. Steinberg" );
-    $ProfSteinberg->addStudent( $Sally );
-
-    $MyUniversity = University::create( "My University", $ProfPipen );
+    $MyUniversity->Decane = $ProfSteinberg;
     $MyUniversity->Teachers->add( $ProfPipen );
     $MyUniversity->Teachers->add( $ProfSteinberg );
     $MyUniversity->Students->add( $Sally );
     $MyUniversity->Students->add( $John );
     $MyUniversity->Students->add( $Michael );
+    $ProfSteinberg->addStudent( $Sally );
+    $ProfPipen->addStudent( $Sally );
+    $ProfPipen->addStudent( $John );
+    $ProfPipen->addStudent( $Michael );
 
-    $Db = $this->getDb();
-    $Db->add( $MyUniversity );
-    $Db->sync();
+    $MyUniversity->save();
+  }
+  
+  function testRead()
+  {
+    $MyUniversity = University::create( $this->Dir, "My University" );
+    
+    $this->assert( $MyUniversity->Decane->Name == "Prof. Pipen" );
+
+    $Michael = $MyUniversity->Students->findFirst( $this->createStudentMatcher( "Michael Jordan" ) );
+    $this->assert( $Michael );
+  }
+
+  function testModify()
+  {
+    $University = University::create( $this->Dir, "My University" );
+    
+    $Michael = $University->Students->findFirst( $this->createStudentMatcher( "Michael Jordan" ) );
+    $this->assert( $Michael );
+    /* @var $Michael Student */
+    $Michael->Name = "Michael Air Jordan";
+    $University->save();
+
+    $University2 = University::create( $this->Dir, "My University" );
+    $Michael2 = $University2->Students->findFirst( $this->createStudentMatcher( "Michael Air Jordan" ) );
+
+    $this->assert( $Michael->Name == $Michael2->Name, $Michael->Name . "is not equals " . $Michael2->Name );
   }
 
   function createStudentMatcher( $name )
@@ -48,66 +71,6 @@ class NodeTest extends \qck\core\abstracts\Test
     {
       return $value instanceof Student && $value->Name == $name;
     };
-  }
-
-  function testRead()
-  {
-    $Db = $this->getDb();
-    /* @var $University University */
-    $University = $Db->get( University::UUID );
-    $this->assert( $University->Decane->Name == "Prof. Pipen" );
-
-    $Michael = $University->Students->findFirst( $this->createStudentMatcher( "Michael Jordan" ) );
-    $this->assert( $Michael );
-  }
-
-  function testModify( $MichaelUuid )
-  {
-    $Db = $this->getDb();
-    /* @var $University University */
-    $University = $Db->get( University::UUID );
-    $Michael = $University->Students->findFirst( $this->createStudentMatcher( "Michael Jordan" ) );
-    $this->assert( $Michael );
-    /* @var $Michael Student */
-    $Michael->Name = "Michael Air Jordan";
-    $Db->sync();
-
-    $NewDatabase = $this->getDb();
-    $University2 = $NewDatabase->get( University::UUID );
-    $Michael2 = $University2->Students->findFirst( $this->createStudentMatcher( "Michael Air Jordan" ) );
-
-    $this->assert( $Michael->Name == $Michael2->Name, $Michael->Name . "is not equals " . $Michael2->Name );
-  }
-
-  function testConcurrentModify()
-  {
-    /* @var $Uni1 University */
-    $Db1 = $this->getFileDatabase();
-    $Uni1 = $Db1->get( University::UUID );
-
-    /* @var $Uni2 University */
-    $Db2 = $this->getFileDatabase();
-    $Uni2 = $Db2->get( University::UUID );
-
-    $Uni1->Students->remove( function($val)
-    {
-      return $val instanceof Student && ($val->Name == "Sally Miller" || $val->Name == "Michael Air Jordan");
-    } );
-
-    $NewStud = Student::create( "Sally The Real Miller" );
-    $Uni2->Students->add( $NewStud );
-    $Db2->add( $NewStud );
-
-    $Db1->sync();
-    $Db2->sync();
-
-    $Db3 = $this->getFileDatabase();
-    $Uni3 = $Db3->get( University::UUID );
-    $SallyMiller = $Uni3->Students->findFirst( $this->createStudentMatcher( "Sally The Real Miller" ) );
-    $this->assert( $SallyMiller );
-
-    $Michael = $Uni3->Students->findFirst( $this->createStudentMatcher( "Michael Air Jordan" ) );
-    $this->assert( $Michael === null );
   }
 
   public function getRequiredTests()
--- GraphStorage/Tests/University.php
+++ GraphStorage/Tests/University.php
@@ -17,12 +17,11 @@ class University extends GraphStorage\Root
 
   const UUID = "2e6a4315-7c94-4086-9c33-c477adac32e7";
 
-  static function create( $DataDir, $Name, Teacher $Decane )
+  static function create( $DataDir, $Name )
   {
     $University = new University( $DataDir );
     $University->Teachers = new GraphStorage\Node();
     $University->Students = new GraphStorage\Node();
-    $University->Decane = $Decane;
     $University->Name = $Name;
     $University->DateFounded = '15-Feb-2009';
     return $University;
