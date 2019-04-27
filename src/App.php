6e262b432d07be5938b0b475fc21095bdf9e8e3e
mueller <mueller@mbits.info>
2019-04-27 12:02:30 +0200

    changes to App Class

--- src/App.php
+++ src/App.php
@@ -33,56 +33,92 @@ abstract class App
 
     protected function setupErrorHandling()
     {
-        error_reporting(E_ALL);
-        ini_set('log_errors', intval(!$this->DevMode));
-        ini_set('display_errors', intval($this->DevMode));
-        ini_set('html_errors', intval(!$this->getCliDetector()->isCli()));
+        error_reporting( E_ALL );
+        ini_set( 'log_errors', intval( !$this->DevMode ) );
+        ini_set( 'display_errors', intval( $this->DevMode ) );
+        ini_set( 'html_errors', intval( !$this->getCliDetector()->isCli() ) );
+
+        set_error_handler( array ( $this, "errorHandler" ) );
+        set_exception_handler( array ( $this, "exceptionHandler" ) );
     }
 
-    function buildUrl($MethodName, array $QueryData = [])
+    function buildUrl( $MethodName, array $QueryData = [] )
     {
-        $CompleteQueryData = array_merge([$this->MethodParamName => $MethodName], $QueryData);
-        return "?" . http_build_query($CompleteQueryData);
+        $CompleteQueryData = array_merge( [ $this->MethodParamName => $MethodName ], $QueryData );
+        return "?" . http_build_query( $CompleteQueryData );
     }
 
-    function handleError($Error, $ReturnCode)
+    function errorHandler( $errno, $errstr, $errfile, $errline )
     {
-        if ($this->getCliDetector()->isCli() == false)
-            http_response_code($ReturnCode);
-        if ($this->DevMode)
-            print $Error;
-        else
-            error_log($Error);
-        exit(-1);
+        throw new \ErrorException( $errstr, 0, $errno, $errfile, $errline );
+    }
+
+    function exceptionHandler( $Exception )
+    {
+        /* @var $Exception \Exception */
+        if ( $this->getCliDetector()->isCli() == false )
+            http_response_code( $Exception->getCode() );
+
+        throw $Exception;
     }
 
     function run()
     {
-
+        $this->setupErrorHandling();
         // find method and run
-        $ShellMethods = $this->getShellMethods();
-        $RequestedMethodName = $this->getInputs()->get($this->MethodParamName, $ShellMethods[0]);
-        if (in_array($RequestedMethodName, $ShellMethods) === false)
-            $this->handleError(sprintf("Method %s is not declared as Shell Method.", $RequestedMethodName), 404);
+        $ShellMethods        = $this->getShellMethods();
+        $RequestedMethodName = $this->getInputs()->get( $this->MethodParamName, $ShellMethods[ 0 ] );
+        if ( in_array( $RequestedMethodName, $ShellMethods ) === false )
+            throw new \Exception ( sprintf( "Method %s is not declared as Shell Method.", $RequestedMethodName ), 404 );
 
-        $Method = new \ReflectionMethod($this, $RequestedMethodName);
-        if ($Method->isPublic() == false)
+        $Method = new \ReflectionMethod( $this, $RequestedMethodName );
+        if ( $Method->isPublic() == false && ($this->AuthOnCli || $this->getCliDetector()->isCli() == false) )
         {
             $MethodAllowed = false;
-            $User = $this->getSession()->getCurrentUser();
-            if ($User)
+            $User          = $this->getSession()->getCurrentUser();
+            if ( $User )
                 $MethodAllowed = $Method->isProtected() || ($Method->isPrivate() && $User->isAdmin());
 
-            if ($MethodAllowed === false)
-                $this->handleError(sprintf("Method %s is not allowed to be called.", $RequestedMethodName), Interfaces\HttpResponder::EXIT_CODE_UNAUTHORIZED);
+            if ( $MethodAllowed === false )
+                throw new \Exception( sprintf( "Method %s is not allowed to be called.", $RequestedMethodName ), Interfaces\HttpResponder::EXIT_CODE_UNAUTHORIZED );
         }
         $RequestedParams = $Method->getParameters();
-        $FoundParams = [];
-        foreach ($RequestedParams as $RequestedParam)
-            $FoundParams[] = $this->getInputs()->get($RequestedParam->getName(), null);
+        $FoundParams     = [];
+        foreach ( $RequestedParams as $RequestedParam )
+            $FoundParams[]   = $this->getInputs()->get( $RequestedParam->getName(), null );
 
-        $Method->setAccessible(true);
-        $Method->invokeArgs($this, $FoundParams);
+        $Method->setAccessible( true );
+        $Method->invokeArgs( $this, $FoundParams );
+    }
+
+    function getDevMode()
+    {
+        return $this->DevMode;
+    }
+
+    function getAuthOnCli()
+    {
+        return $this->AuthOnCli;
+    }
+
+    function getMethodParamName()
+    {
+        return $this->MethodParamName;
+    }
+
+    function setDevMode( $DevMode )
+    {
+        $this->DevMode = $DevMode;
+    }
+
+    function setAuthOnCli( $AuthOnCli )
+    {
+        $this->AuthOnCli = $AuthOnCli;
+    }
+
+    function setMethodParamName( $MethodParamName )
+    {
+        $this->MethodParamName = $MethodParamName;
     }
 
     /**
@@ -93,8 +129,14 @@ abstract class App
 
     /**
      *
+     * @var bool 
+     */
+    protected $AuthOnCli = true;
+
+    /**
+     *
      * @var string
      */
-    protected $MethodParamName = "function";
+    protected $MethodParamName = "q";
 
 }
