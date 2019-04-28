8477bf4c575e89186904b17cd3e57a54fbc5ceab
mueller <mueller@mbits.info>
2019-04-28 19:40:31 +0200

    Checkpoint commit as of 2019-04-28-19-40-24.

--- src/DefaultAuthenticator.php
+++ src/DefaultAuthenticator.php
@@ -9,52 +9,52 @@ namespace Qck;
 class DefaultAuthenticator implements \Qck\Interfaces\Authenticator
 {
 
-  function __construct( \Qck\Interfaces\UserDb $UserDb, \Qck\Interfaces\PasswordHasher $PasswordHasher, \Qck\Interfaces\Session $Session )
-  {
-    $this->UserDb         = $UserDb;
-    $this->PasswordHasher = $PasswordHasher;
-    $this->Session        = $Session;
-  }
-
-  public function check( $Username, $PlainTextPassword )
-  {
-    $CredentialsOk = false;
-    $User          = $this->UserDb->getUser( $Username );
-
-    if ( $User )
+    function __construct( \Qck\Interfaces\UserDb $UserDb, \Qck\Interfaces\PasswordHasher $PasswordHasher, \Qck\Interfaces\Session $Session )
     {
-      // Use a custom Authenticator?
-      $Authenticator = $User->getAuthenticator();
-
-      if ( $Authenticator )
-        $CredentialsOk = $Authenticator->check( $Username, $PlainTextPassword );
-      else
-        $CredentialsOk = $this->PasswordHasher->verify( $PlainTextPassword, $User->getHashedPassword() );
-    }
-    if ( $CredentialsOk )
-    {
-      $this->Session->setUsername( $Username );
+        $this->UserDb         = $UserDb;
+        $this->PasswordHasher = $PasswordHasher;
+        $this->Session        = $Session;
     }
 
-    return $CredentialsOk;
-  }
+    public function check( $Username, $PlainTextPassword )
+    {
+        $CredentialsOk = false;
+        $User          = $this->UserDb->getUser( $Username );
 
-  /**
-   *
-   * @var \Qck\Interfaces\UserDb
-   */
-  protected $UserDb;
+        if ( $User )
+        {
+            // Use a custom Authenticator?
+            $Authenticator = $User->getAuthenticator();
 
-  /**
-   *
-   * @var \Qck\Interfaces\PasswordHasher
-   */
-  protected $PasswordHasher;
+            if ( $Authenticator )
+                $CredentialsOk = $Authenticator->check( $Username, $PlainTextPassword );
+            else
+                $CredentialsOk = $this->PasswordHasher->verify( $PlainTextPassword, $User->getHashedPassword() );
+        }
+        if ( $CredentialsOk )
+        {
+            $this->Session->startSession( $Username );
+        }
 
-  /**
-   *
-   * @var \Qck\Interfaces\Session
-   */
-  protected $Session;
+        return $CredentialsOk;
+    }
+
+    /**
+     *
+     * @var \Qck\Interfaces\UserDb
+     */
+    protected $UserDb;
+
+    /**
+     *
+     * @var \Qck\Interfaces\PasswordHasher
+     */
+    protected $PasswordHasher;
+
+    /**
+     *
+     * @var \Qck\Interfaces\Session
+     */
+    protected $Session;
 
 }
