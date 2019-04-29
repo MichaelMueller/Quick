38d0d6ef00aed5eab9d0a5044ad9e5c7121e0ae1
mueller <mueller@mbits.info>
2019-04-29 07:38:50 +0200

    Checkpoint commit as of 2019-04-29-07-38-44.

--- src/User.php
+++ src/User.php
@@ -6,7 +6,7 @@ namespace Qck;
  *
  * @author muellerm
  */
-class User implements \Qck\Interfaces\User, Interfaces\Serialization\Serializable
+class User implements \Qck\Interfaces\User
 {
 
     function setUsername( $Username )
@@ -47,26 +47,6 @@ class User implements \Qck\Interfaces\User, Interfaces\Serialization\Serializabl
     public function isAdmin()
     {
         return $this->Admin;
-    }
-
-    public function fromScalarArray( array $ScalarArray,
-                                     Interfaces\Serialization\Source $Source,
-                                     $Reload = false )
-    {
-        $this->Username       = $ScalarArray[ 0 ];
-        $this->HashedPassword = $ScalarArray[ 1 ];
-        $this->Authenticator  = $ScalarArray[ 2 ];
-        $this->Admin          = $ScalarArray[ 3 ];
-    }
-
-    public function getOwnedObjects()
-    {
-        return [];
-    }
-
-    public function toScalarArray( Interfaces\Serialization\ObjectIdProvider $ObjectIdProvider )
-    {
-        return [ $this->Username, $this->HashedPassword, $this->Authenticator, $this->Admin ];
     }
 
     protected $Username;
