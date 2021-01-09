3809ba1b4bc9ae8098c3d9e060e01b487206518e
mueller <mueller@mbits.info>
2021-01-09 22:27:14 +0100

    Checkpoint commit 20210109_101001011414

--- src/deploy.php
+++ src/deploy.php
@@ -6,37 +6,39 @@ class Deploy implements Qck\Interfaces\AppFunction
 {
 
     public function run( \Qck\Interfaces\App $app )
-    {        
-        $exception      = $app->createException();
+    {
+        $exception     = $app->createException();
         $cmd           = $app->createCmd( "git" )->arg( "add" )->arg( "-A" );
         $exception->assert( $cmd->run()->successful(), "failed to run git add" );
         $dateTime      = (new DateTime() )->format( "Ymd_hhmmss" );
         $commitMessage = "Checkpoint commit " . $dateTime;
         $cmd           = $app->createCmd( "git" )->arg( "commit" )->arg( "-m" )->escapeArg( $commitMessage );
         $exception->assert( $cmd->run()->successful(), "failed to run git commit" );
-        
-        $interfaceFiles = glob( __DIR__ . "/Interfaces/*.php", GLOB_BRACE );
+
+        $interfaceFiles       = glob( __DIR__ . "/Interfaces/*.php", GLOB_BRACE );
         print_r( "found the following interface files" . print_r( $interfaceFiles, true ) );
         $namespaceDeclaration = "namespace \\Qck\\Interfaces;";
-        $newContents = PHP_EOL."if(interface_exists()==false)".PHP_EOL;
-        $newContents .= "{".PHP_EOL;
-        $newContents .= $namespaceDeclaration.PHP_EOL;
+        $newContents          = PHP_EOL . "if(interface_exists()==false)" . PHP_EOL;
+        $newContents          .= "{" . PHP_EOL;
+        $newContents          .= $namespaceDeclaration . PHP_EOL;
         foreach ( $interfaceFiles as $interfaceFile )
         {
-            $fileContents = file_get_contents($interfaceFile);
-            $fileContents = str_replace("<?php", "", $fileContents);
-            $fileContents = str_replace($namespaceDeclaration, "", $fileContents);
-            $fileContents = trim($fileContents);
-            $newContents .= $fileContents.PHP_EOL;
+            $fileContents = file_get_contents( $interfaceFile );
+            $fileContents = str_replace( "<?php", "", $fileContents );
+            $fileContents = str_replace( $namespaceDeclaration, "", $fileContents );
+            $fileContents = trim( $fileContents );
+            $newContents  .= $fileContents . PHP_EOL;
         }
-        $newContents .= "}".PHP_EOL;
-        
-        $appFile = __DIR__."/App.php";
-        $appFileContents = file_get_contents($appFile);
-        $find = strpos($appFileContents, $namespaceDeclaration);
-        $classesPart = substr($appFileContents, 0, $find);
-        file_put_contents($appFile, $classesPart.$newContents);
-        
+        $newContents .= "}" . PHP_EOL;
+
+        $appFile         = __DIR__ . "/App.php";
+        $appFileContents = file_get_contents( $appFile );
+        $find            = strpos( $appFileContents, $namespaceDeclaration );
+        if ( $find !== false )
+            $classesPart     = substr( $appFileContents, 0, $find );
+        else 
+            $classesPart = $appFileContents; 
+        file_put_contents( $appFile, $classesPart . $newContents );
     }
 
 }
